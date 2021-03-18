2105c47374997b1aeea67192a54a99b838466110
xiaoqin <Lanxiaoqinhelen@gmail.com>
2021-03-18 13:09:43 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jordan Liu on 2021-03-18.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
